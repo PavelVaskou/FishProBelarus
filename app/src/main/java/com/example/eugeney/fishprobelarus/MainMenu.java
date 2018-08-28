8473c1f7e82e5f3521acef9420a4e317b64e8d78
EugeneyC <xxtedixx@yandex.ru>
2018-08-28 09:36:12 +0300

    Частично настроено меню. Добалены иконки

--- app/src/main/java/com/example/eugeney/fishprobelarus/MainMenu.java
+++ app/src/main/java/com/example/eugeney/fishprobelarus/MainMenu.java
@@ -2,12 +2,47 @@ package com.example.eugeney.fishprobelarus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainMenu extends AppCompatActivity {
+    final String LOG_TAG = "myLogs";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    ListView lvMain;
+
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+
+        lvMain = findViewById(R.id.lvMain);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.names, android.R.layout.simple_list_item_1);
+        lvMain.setAdapter(adapter);
+
+        lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Log.d(LOG_TAG, "itemClick: position = " + position + ", id = "
+                        + id);
+            }
+        });
+
+        lvMain.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.d(LOG_TAG, "itemSelect: position = " + position + ", id = "
+                        + id);
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d(LOG_TAG, "itemSelect: nothing");
+            }
+        });
+
     }
 }
