22a8d0a73244208722a0880b3f1b9db6c19d5000
EugeneyC <xxtedixx@yandex.ru>
2018-08-31 22:06:30 +0300

    Добавлены активити для каждого пункта меню

--- app/src/main/java/com/example/eugeney/fishprobelarus/NavigationActivity.java
+++ app/src/main/java/com/example/eugeney/fishprobelarus/NavigationActivity.java
@@ -1,5 +1,6 @@
 package com.example.eugeney.fishprobelarus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -71,17 +72,24 @@ public class NavigationActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_reservoirs) {
+            Intent intent = new Intent(this, Reservoirs.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_fish) {
+            Intent intent = new Intent(this, FishList.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_rigging) {
+            Intent intent = new Intent(this, Rigging.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_maps) {
+            Intent intent = new Intent(this, Maps.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_useful) {
+            Intent intent = new Intent(this, Useful.class);
+            startActivity(intent);
+        }else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        }else if (id == R.id.nav_send) {
 
         }
 
---
+++ app/src/main/java/com/example/eugeney/fishprobelarus/Rigging.java
@@ -0,0 +1,13 @@
+package com.example.eugeney.fishprobelarus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Rigging extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rigging);
+    }
+}
